bb1560b857edd682aaa89d5460d798df1c26a2c8
Joshua Lorincz <joshlordeveloper@gmail.com>
2019-06-12 17:28:17 -0700

    Initial commit of single agent path finding files

---
+++ single_agent_pathfinding/runner.py
@@ -0,0 +1,7 @@
+from classes.agent_source.agent import Agent
+from classes.simple_objects.position import Position
+from classes.simple_objects.shape import Shape
+
+
+def pathfinder(position_array: [Position], shape: Shape, environment):
+    return environment
