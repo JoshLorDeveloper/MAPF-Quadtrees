3bee050cc3b88980b7d34ed4732f5152f58f77e6
Joshua Lorincz <joshlordeveloper@gmail.com>
2019-06-13 12:29:05 -0700

    added note

---
+++ single_agent_pathfinding/pathfinder.py
@@ -0,0 +1,9 @@
+from classes.agent_source.agent import Agent
+from classes.simple_objects.position import Position
+from classes.simple_objects.shape import Shape
+
+
+# can implement if it is decided that using high level pathfinding algorithm to decide paths is much slower
+
+def pathfinder(start_position: Position, goal_position: Position, shape: Shape, environment):
+    return environment
--- single_agent_pathfinding/runner.py
+++
@@ -1,7 +0,0 @@
-from classes.agent_source.agent import Agent
-from classes.simple_objects.position import Position
-from classes.simple_objects.shape import Shape
-
-
-def pathfinder(position_array: [Position], shape: Shape, environment):
-    return environment
