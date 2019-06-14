6b41171eb5bb0d9010c8e5024b8fe08d7e46b014
Joshua Lorincz <joshlordeveloper@gmail.com>
2019-06-14 14:03:41 -0700

    added environment class descriptions

--- setup/environment_setup.py
+++ setup/environment_setup.py
@@ -3,6 +3,7 @@ from classes.simple_objects.range import Range
 from classes.simple_objects.shape import Shape
 
 
+# class for defining creation of obstacles or restricted areas
 class Obstacle:
 
     def __init__(self, position: Position, shape: Shape):
@@ -10,6 +11,7 @@ class Obstacle:
         self.shape = shape
 
 
+# class for defining creation of enviroment that agents will operate in
 class Environment:
 
     def __init__(self, range_x: Range, range_y: Range, obstacles: [Obstacle]):
@@ -18,9 +20,11 @@ class Environment:
         self.obstacles = obstacles
 
 
+# class for defining creation of agents, from start and end position
 class AgentInitializer:
 
-    def __init__ (self, start_position:Position, end_position:Position, shape:Shape):
+    def __init__ (self, start_position:Position, end_position:Position, shape:Shape, max_speed):
         self.start_position = start_position
         self.end_position = end_position
         self.shape = shape
+        self.max_speed = max_speed
